b446a68652a834917958746d4390788635b83fdb
AbiodunOlaoluwa <mate.olaoluwa@gmail.com>
2024-03-25 12:44:29 +0100

    Gotten started with the layout and the structure

--- to-do_list/src/App.js
+++ to-do_list/src/App.js
@@ -2,7 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="app"></div>
+    <div className="app">
+      <div className="headerImage">
+        <div className="headerImage"></div>
+        <div className="activeContainer">
+          <h2>This is a test</h2>
+        </div>
+      </div>
+    </div>
   );
 }
 
