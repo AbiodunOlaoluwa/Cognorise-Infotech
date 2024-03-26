cd1a1707c555d422f233056c02ae3a9421e5c4b1
AbiodunOlaoluwa <mate.olaoluwa@gmail.com>
2024-03-27 00:39:37 +0100

    Finished the app for the internship requirments, proceeding to make video

--- to-do_list/src/App.js
+++ to-do_list/src/App.js
@@ -11,15 +11,26 @@ function App() {
   useEffect(() => {
     const savedTasks = localStorage.getItem('tasks');
     if (savedTasks) {
-      setTaskArray(JSON.parse(savedTasks));
+      const parsedTasks = JSON.parse(savedTasks);
+      setTaskArray(parsedTasks);
+    }
+    else {
+      setTaskArray([]);
     }
   }, []);
 
+  // useEffect(() => {
+  //   localStorage.setItem('tasks', JSON.stringify(taskArray));
+  // }, [taskArray])
+
   const addTask = () => {
-    const newTaskArray = [...taskArray, taskText];
-    setTaskArray(newTaskArray)
-    setTaskText("");
-    localStorage.setItem('tasks', JSON.stringify(newTaskArray));
+    if (taskText.trim() !== "") {
+      const newTaskArray = [...taskArray, { text: taskText, completed: false }];
+      setTaskArray(newTaskArray)
+      setTaskText("");
+      localStorage.setItem('tasks', JSON.stringify(newTaskArray));
+    }
+    // localStorage.setItem('tasks', JSON.stringify(taskArray))
   };
 
   const deleteTask = (id) => {
@@ -36,6 +47,20 @@ function App() {
     if (event.key === "Enter") {
       addTask();
     }
+  }
+
+  const toggleTaskCompletion = (id) => {
+    setTaskArray(prevValue => {
+      return prevValue.map((item, index) => {
+        if (index === id) {
+          return { ...item, completed: !item.completed };
+        }
+        return item;
+      })
+    });
+    const updatedTasks = taskArray.map((task, index) => ({ ...task })); // Create a copy
+    updatedTasks[id].completed = !updatedTasks[id].completed; // Update completion status in copy
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
   }
 
 
@@ -55,13 +80,13 @@ function App() {
                 <input type="text" name="todo" className="todoInput" id="todo" autoFocus autoCapitalize='words' placeholder="Create a new todo..." autoComplete='off' value={taskText.charAt(0).toUpperCase() + taskText.slice(1)} onChange={(event) => setTaskText(event.target.value)} onKeyDown={handleKeyDown} />
                 {/* <button className="addTodoButton" type="button" onClick={addTask}>Add</button> */}
                 <div className="addTodoButton" onClick={addTask}>
-                <svg className="addCross" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
+                  <svg className="addCross" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z" /></svg>
                 </div>
               </div>
               <div className="listContainer">
                 {taskArray.map((task, index) => {
                   return (
-                    <TodoListItem key={index} id={index} text={task} delete={deleteTask} />
+                    <TodoListItem key={index} id={index} text={task.text} delete={deleteTask} completed={task.completed} toggleCompletion={toggleTaskCompletion} />
                   );
                 })}
               </div>
--- to-do_list/src/todoListItem.jsx
+++ to-do_list/src/todoListItem.jsx
@@ -6,11 +6,15 @@ const TodoListItem = (props) => {
         props.delete(props.id)
     }
 
+    const handleDoneClick = () => {
+        props.toggleCompletion(props.id)
+    }
+
     return (
         <div className="todoListItem">
             <div className="taskTextContainer">
-            <input type="checkbox" name="completeMarker1" className="completed" />
-            <p className="todoListText">{props.text}</p>
+            {/* <input type="checkbox" name="completeMarker1" className="completed" /> */}
+            <p className={props.completed ? "strikeOver todoListText" : "todoListText"} onClick={handleDoneClick}>{props.text}</p>
             </div>
             <div className="deleteButton" onClick={handleDelClick}>
                 <svg className="delIcon" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z" /></svg>
