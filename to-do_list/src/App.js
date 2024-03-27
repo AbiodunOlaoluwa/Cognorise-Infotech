4f10e430dedbec765ee5bef52f7203513eaa4ab3
AbiodunOlaoluwa <mate.olaoluwa@gmail.com>
2024-03-27 12:35:53 +0100

    Finally finished it, added the filters to make it even more functional

--- to-do_list/src/App.js
+++ to-do_list/src/App.js
@@ -7,6 +7,9 @@ function App() {
 
   const [taskArray, setTaskArray] = useState([]);
   const [taskText, setTaskText] = useState("");
+  const [filterStatus, setFilterStatus] = useState("all");
+  const [renderedTaskArray, setRenderedTaskArray] = useState([]);
+  const [itemsNum, setItemsNum] = useState("");
 
   useEffect(() => {
     const savedTasks = localStorage.getItem('tasks');
@@ -18,6 +21,29 @@ function App() {
       setTaskArray([]);
     }
   }, []);
+
+  useEffect(() => {
+    if (filterStatus === "all") {
+      setRenderedTaskArray(taskArray);
+    }
+    else if (filterStatus === "active") {
+      setRenderedTaskArray( taskArray.filter((task, id) => {
+        return (task.completed === false);
+      }))
+    }
+    else if (filterStatus === "completed") {
+      setRenderedTaskArray(taskArray.filter((task, id) => {
+        return (task.completed === true);
+      }))
+    }
+  }, [filterStatus, taskArray])
+
+  useEffect(() => {
+    const activeTasks = taskArray.filter((task) => {
+      return (task.completed === false);
+    })
+    setItemsNum(activeTasks.length);
+  }, [taskArray])
 
   const addTask = () => {
     if (taskText.trim() !== "") {
@@ -54,9 +80,20 @@ function App() {
         return item;
       })
     });
+    // the copy made below is necessary, it is the only way for the completed property to be updated and sent to the localStorage DB
     const updatedTasks = taskArray.map((task, index) => ({ ...task })); // Create a copy
     updatedTasks[id].completed = !updatedTasks[id].completed; // Update completion status in copy
     localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+  }
+
+  const handleClearCompleted = () => {
+    setTaskArray(prevValue => {
+      const updatedTasks = prevValue.filter((item) => {
+        return item.completed !== true;
+      });
+      localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+      return updatedTasks;
+    })
   }
 
 
@@ -74,17 +111,35 @@ function App() {
             <div className="listBody">
               <div className="addNewTask">
                 <input type="text" name="todo" className="todoInput" id="todo" autoFocus autoCapitalize='words' placeholder="Create a new todo..." autoComplete='off' value={taskText.charAt(0).toUpperCase() + taskText.slice(1)} onChange={(event) => setTaskText(event.target.value)} onKeyDown={handleKeyDown} />
-                {/* <button className="addTodoButton" type="button" onClick={addTask}>Add</button> */}
                 <div className="addTodoButton" onClick={addTask}>
                   <svg className="addCross" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z" /></svg>
                 </div>
               </div>
               <div className="listContainer">
-                {taskArray.map((task, index) => {
+                {renderedTaskArray.map((task, index) => {
                   return (
                     <TodoListItem key={index} id={index} text={task.text} delete={deleteTask} completed={task.completed} toggleCompletion={toggleTaskCompletion} />
                   );
                 })}
+              </div>
+              <div className="filtersContainer">
+                <div className="remainingItemsIndicator">
+                  <p>{itemsNum} items left</p>
+                </div>
+                <div className="filtersIcons">
+                  <div className={filterStatus === "all" ? "all activeFilter" : "all"} onClick={() => setFilterStatus("all")}>
+                    <p>All</p>
+                  </div>
+                  <div className={filterStatus === "active" ? "active activeFilter" : "active"} onClick={() => setFilterStatus("active")}>
+                    <p>Active</p>
+                  </div>
+                  <div className={filterStatus === "completed" ? "completed activeFilter" : "completed"} onClick={() => setFilterStatus("completed")}>
+                    <p>Completed</p>
+                  </div>
+                </div>
+                <div className="clearCompleted" onClick={handleClearCompleted}>
+                  <p>Clear Completed</p>
+                </div>
               </div>
             </div>
           </div>
