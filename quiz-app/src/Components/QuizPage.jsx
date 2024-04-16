2528cefc0d5927c942f72f6836302ac13287f7f5
AbiodunOlaoluwa <mate.olaoluwa@gmail.com>
2024-04-16 21:28:41 +0100

    I'm done

--- quiz-app/src/Components/QuizPage.jsx
+++ quiz-app/src/Components/QuizPage.jsx
@@ -15,6 +15,9 @@ const QuizPage = () => {
   const [userScore, setUserScore] = useState(0);
   const [optionSelected, setOptionSelected] = useState(false);
   const [selectedAnswer, setSelectedAnswer] = useState("");
+
+  // const correctIcon = <svg className='correctIcon' xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 40 40"><path fill="#26D782" d="M20 5a15 15 0 1 1 0 30 15 15 0 0 1 0-30Zm0 2.5a12.5 12.5 0 1 0 0 25 12.5 12.5 0 0 0 0-25Zm-1.875 15.105L25.3 15.41a1.25 1.25 0 0 1 1.915 1.593l-.145.174-8.06 8.08a1.25 1.25 0 0 1-1.595.148l-.175-.145-4.375-4.375a1.25 1.25 0 0 1 1.595-1.913l.175.143 3.49 3.49Z"/></svg>;
+  // const incorrectIcon = <svg className='incorrectIcon' xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 40 40"><path fill="#EE5454" d="M20 5a15 15 0 1 1 0 30 15 15 0 0 1 0-30Zm0 2.5a12.5 12.5 0 1 0 0 25 12.5 12.5 0 0 0 0-25Zm-5.402 7.415.142-.175a1.25 1.25 0 0 1 1.595-.143l.175.143L20 18.233l3.49-3.493a1.25 1.25 0 0 1 1.595-.143l.175.143a1.25 1.25 0 0 1 .142 1.595l-.142.175L21.767 20l3.493 3.49a1.25 1.25 0 0 1 .142 1.595l-.142.175a1.25 1.25 0 0 1-1.595.142l-.175-.142L20 21.767l-3.49 3.493a1.25 1.25 0 0 1-1.595.142l-.175-.142a1.25 1.25 0 0 1-.143-1.595l.143-.175L18.233 20l-3.493-3.49a1.25 1.25 0 0 1-.143-1.595Z"/></svg>;
     
   useEffect(() => {
 
@@ -64,13 +67,22 @@ const QuizPage = () => {
 
     const currentQuestion = questions[currentQuestionIndex];
 
-    const handleAnswerSelect = (option) => {
+    let optionBoxClassNames = Array(currentQuestion.options.length).fill("optionBox");
+
+    const handleAnswerSelect = (option, index) => {
       setSelectedAnswer(option);
+      const newClassNames = [...optionBoxClassNames];
+      newClassNames[index] = option === currentQuestion.answer ? "optionBox correctOptionBox" : "optionBox incorrectOptionBox";
+      optionBoxClassNames = newClassNames;
+    }
+
+    const navigateToScorePage = () => {
+      navigate('/scorePage', {state: {score: userScore}});
     }
 
     const handleNextQuestionClick = () => {
       if (currentQuestionIndex === quiz.questions.length - 1) {
-        navigate('/scorePage', {replace:true}, {state: {score: userScore}});
+        setTimeout(navigateToScorePage, 1000)
       }
       else {
         setCurrentQuestionIndex(currentQuestionIndex + 1);
@@ -120,8 +132,8 @@ const QuizPage = () => {
           <div className="answerOptionsContainer">
             <div className="optionsContainer">
             {currentQuestion.options.map((option, index) => (
-              <div className="optionBox" key={index} onClick={() => {
-                handleAnswerSelect(option)
+              <div className={optionBoxClassNames[index]} key={index} onClick={() => {
+                handleAnswerSelect(option, index)
               }}>
                 <p className="optionBoxText">{option}</p>
               </div>
--- quiz-app/src/Components/ScorePage.jsx
+++ quiz-app/src/Components/ScorePage.jsx
@@ -1,12 +1,64 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import "./scorePage.css"
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const ScorePage = () => {
 
+    const navigate = useNavigate();
     const {state} = useLocation();
+    const score = state.score;
+    const icon = state.quizIcon;
+    const topic = state.topic;
+
+
+
+    const handlePlayAgainClick = () => {
+        navigate('/');
+    }
 
   return (
-    <div>{state.score}</div>
+    <div className="pageContainer">
+        <div className="firstCurve"></div>
+        <div className="secondCurve"></div>
+        <div className="mainContent">
+            <div className="quizIndicator">
+                <div className="indicatorContent">
+                    <div className="iconContainer">
+                        {icon}
+                    </div>
+                    <div className="indicatorText">
+                        {topic}
+                    </div>
+                </div>
+                <div className="swithTheme"></div>
+            </div>
+            <div className="textIntroduction">
+                <div className="introTextContainer">
+                    <h1 className="introText">Quiz Completed</h1>
+                    <h1 className="introTextSecondLine">You Scored...</h1>
+                </div>
+            </div>
+            <div className="optionsContainer">
+                <div className="scoreDisplayContainer">
+                    <div className="scoreTextContainer">
+                        <div className="scoreText">
+                            <p>
+                                {score}
+                            </p>
+                            <div className="instructionText">out of 10</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="submitContainer">
+                    <div className="submitTextContainer" onClick={handlePlayAgainClick}>
+                        <p className="submitText">
+                            Play Again
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
